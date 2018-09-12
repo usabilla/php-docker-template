1ffc0157dd0bcf794b84ed47c39fa5bfade5d426
Renato Mefi <gh@mefi.in>
2018-09-12 08:53:49 +0200

    Fix python scripts indentation
    Accordingly to our standards it should be 4 spaces, it passed unnoticed
    the last time.

--- test/container/test_helper_scripts.py
+++ test/container/test_helper_scripts.py
@@ -2,42 +2,43 @@ import pytest
 
 @pytest.mark.php
 def test_php_images_contain_helper_scripts(host):
-  helper_scripts = [
-    "/usr/local/bin/docker-php-source-tarball",
-    "/usr/local/bin/docker-php-source",
-    "/usr/local/bin/docker-php-ext-install",
-    "/usr/local/bin/docker-php-ext-enable",
-    "/usr/local/bin/docker-php-ext-configure",
-    "/usr/local/bin/docker-php-entrypoint",
-  ]
+    helper_scripts = [
+        "/usr/local/bin/docker-php-source-tarball",
+        "/usr/local/bin/docker-php-source",
+        "/usr/local/bin/docker-php-ext-install",
+        "/usr/local/bin/docker-php-ext-enable",
+        "/usr/local/bin/docker-php-ext-configure",
+        "/usr/local/bin/docker-php-entrypoint",
+        "/usr/local/bin/php-fpm-healthcheck",
+    ]
 
-  for file in helper_scripts:
-    assert host.file(file).exists is True
-    assert host.file(file).is_file is True
-    assert host.file(file).mode == 0o775
+    for file in helper_scripts:
+        assert host.file(file).exists is True
+        assert host.file(file).is_file is True
+        assert host.file(file).mode == 0o775
 
 @pytest.mark.php
 def test_php_source_tarball_script(host):
-  assert host.file("/usr/src/php.tar.xz").exists is False
-  assert host.file("/usr/src/php.tar.xz.asc").exists is False
-  assert host.file("/usr/src/php").exists is False
+    assert host.file("/usr/src/php.tar.xz").exists is False
+    assert host.file("/usr/src/php.tar.xz.asc").exists is False
+    assert host.file("/usr/src/php").exists is False
 
-  host.run("apk add --no-cache gnupg")
-  host.run("docker-php-source-tarball download")
-  assert host.file("/usr/src/php.tar.xz").exists is True
-  assert host.file("/usr/src/php.tar.xz.asc").exists is True
-  assert host.file("/usr/src/php").exists is False
+    host.run("apk add --no-cache gnupg")
+    host.run("docker-php-source-tarball download")
+    assert host.file("/usr/src/php.tar.xz").exists is True
+    assert host.file("/usr/src/php.tar.xz.asc").exists is True
+    assert host.file("/usr/src/php").exists is False
 
-  host.run("docker-php-source extract")
-  assert host.file("/usr/src/php").exists is True
+    host.run("docker-php-source extract")
+    assert host.file("/usr/src/php").exists is True
 
-  host.run("docker-php-source-tarball delete")
-  assert host.file("/usr/src/php.tar.xz").exists is False
-  assert host.file("/usr/src/php.tar.xz.asc").exists is False
-  assert host.file("/usr/src/php").exists is True
+    host.run("docker-php-source-tarball delete")
+    assert host.file("/usr/src/php.tar.xz").exists is False
+    assert host.file("/usr/src/php.tar.xz.asc").exists is False
+    assert host.file("/usr/src/php").exists is True
 
-  host.run("docker-php-source-tarball clean")
-  assert host.file("/usr/src/php").exists is False
+    host.run("docker-php-source-tarball clean")
+    assert host.file("/usr/src/php").exists is False
 
 @pytest.mark.php_fpm
 def test_php_fpm_status_is_enabled(host):
