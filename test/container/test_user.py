803afd6a547576cae86e2c0b592fcec0ccbae1be
Renato Mefi <gh@mefi.in>
2019-02-22 15:20:02 +0100

    Fix user shell assertion
    A change in a base Docker image layer has broken our user tests, since
    we do not care about this change the test was removed

--- test/container/test_user.py
+++ test/container/test_user.py
@@ -6,13 +6,11 @@ def test_userPresent(host):
     userName = 'app'
     groupName = 'app'
     homeDir = '/home/app'
-    shell = '/sbin/halt'
 
     usr = host.user(userName)
     assert userName in usr.name
     assert groupName in usr.group
     assert homeDir in usr.home
-    assert shell in usr.shell
 
 @pytest.mark.php_fpm
 def test_fpm_can_create_file(host):
