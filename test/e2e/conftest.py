626c3fe5c8c779715af1651b28aa1aad8b00a153
Renato Mefi <gh@mefi.in>
2019-09-04 19:15:31 +0200

    Support custom env vars on docker run test

--- test/e2e/conftest.py
+++ test/e2e/conftest.py
@@ -12,10 +12,19 @@ def tag(request):
     return request.config.getoption("--tag")
 
 @pytest.fixture
-def container(host, tag):
-    container = host.check_output('docker run -p 80 -d {}'.format(tag))
+def container(host, tag, request):
+    port = '80'
+    run_flags = ''
+
+    if hasattr(request, 'param') and type(request.param) is dict:
+        if 'port' in request.param:
+            port = request.param['port']
+        if 'env' in request.param and type(request.param['env']) is dict:
+            run_flags = run_flags.join("-e {}={} ".format(k, v) for (k, v) in request.param['env'].items())
+
+    container = host.check_output('docker run -p {} {} -d {}'.format(port, run_flags, tag))
     yield container
     host.check_output('docker stop {}'.format(container))
-    
-    # Remove afterwads thus the tests still have access to the logs
+
+    # Remove afterwards thus the tests still have access to the logs
     host.check_output('docker rm -f {}'.format(container))
