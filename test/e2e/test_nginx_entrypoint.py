74ef508ab570088dc58e64c97a5b569c1396fa83
Renato Mefi <gh@mefi.in>
2019-09-04 19:15:52 +0200

    Test custom port on nginx entrypoint

--- test/e2e/test_nginx_entrypoint.py
+++ test/e2e/test_nginx_entrypoint.py
@@ -11,9 +11,17 @@ def test_nginx_sigterm_handling(host, container):
     assert nginx_reload.rc is 0
 
     nginx_stop = host.run('docker stop -t 3 {}'.format(container))
-    assert nginx_reload.rc is 0
+    assert nginx_stop.rc is 0
 
     logs = host.run('docker logs {}'.format(container))
 
     assert u'signal 15 (SIGTERM) received, exiting' in logs.stderr
     assert u'exit' in logs.stderr
+
+
+@pytest.mark.nginx_e2e
+@pytest.mark.parametrize('container', [{'env': {'NGINX_PORT': '5556'}, 'port': '5556'}], indirect=True)
+def test_nginx_can_host_different_ports(host, container):
+    wget_custom_port = host.run('docker exec -t {} sh -c "wget http://127.0.0.1:5556/"'.format(container))
+    assert wget_custom_port.rc is 1
+    assert u'502 Bad Gateway' in wget_custom_port.stdout
