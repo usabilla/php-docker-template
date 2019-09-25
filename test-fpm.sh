57f502716c054b9d6e3c760202d88df23f41c45f
Renato Mefi <gh@mefi.in>
2019-09-25 10:25:53 +0200

    Test PHP-FPM configuration templating

--- test-fpm.sh
+++ test-fpm.sh
@@ -16,7 +16,7 @@ declare TEST_SUITE
 if [[ $DOCKER_TAG == *"-dev" ]]; then
     TEST_SUITE="php or php_fpm or php_dev"
 else
-    TEST_SUITE="php or php_fpm or php_no_dev and not php_dev"
+    TEST_SUITE="php or php_fpm or php_fpm_exec or php_no_dev and not php_dev"
 fi
 
 printf "Starting a container for '%s'\\n" "$DOCKER_TAG"
