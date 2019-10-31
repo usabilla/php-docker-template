8733329c7572febca8628b10653d7e12da0b5d6b
Gabriel Caruso <carusogabriel34@gmail.com>
2019-10-31 14:47:11 +0100

    Use PHP 7.3 and Alpine 3.10 during tests

--- test/functional/web/index.php
+++ test/functional/web/index.php
@@ -1,13 +1,13 @@
 <?php
 
-$testFile = isset($_GET['testFile']) && $_GET['testFile'] === 'true' ? true : false;
+$testFile = isset($_GET['testFile']) && $_GET['testFile'] === 'true';
 
 if ($testFile === true) {
     echo '<h2>whoami? ' . shell_exec('whoami') . '</h2>';
 
     $file = new \SplFileObject('/opt/project/public/tmp/temptestfile', "w");
     $written = $file->fwrite('readable content');
-    
+
     echo "Wrote $written bytes to file: " . $file->getPathname();
 
     exit;
