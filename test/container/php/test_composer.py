b79cad653242b42b9eaa970422d984b9f938e7dd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-11-29 14:52:46 +0100

    Revert: Move composer to -dev images
    This change is causing problems downstream, we'll revisit this in the
    future.

--- test/container/php/test_composer.py
+++ test/container/php/test_composer.py
@@ -1,15 +1,8 @@
 import pytest
 
 @pytest.mark.php_dev
+@pytest.mark.php_no_dev
 def test_php_composer_is_available_only_on_dev_images(host):
     composer = host.run("composer")
     assert composer.rc == 0
     assert "Available commands:" in composer.stdout
-
-@pytest.mark.php_no_dev
-def test_php_composer_isnt_available_on_non_dev_images(host):
-    composer = host.run("composer")
-    assert composer.rc == 127
-    assert "composer" in composer.stderr
-    assert "not found" in composer.stderr
-    assert "" == composer.stdout
